d827a598fefd73bf8224c00528bc7a5adaa50bba
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-03 14:40:36 -0300

    First commit

---
+++ src/main/java/br/uefs/larsid/iot/soft/App.java
@@ -0,0 +1,25 @@
+package br.uefs.larsid.iot.soft;
+
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+  public static void main(String[] args) {
+    try (PythonInterpreter pyInterp = new PythonInterpreter()) {
+        pyInterp.exec("print('Hello World')");
+        pyInterp.exec("import sys");
+        pyInterp.exec("print sys");
+        pyInterp.set("a", new PyInteger(42));
+        pyInterp.exec("print a");
+        pyInterp.exec("x = 2+2");
+        PyObject x = pyInterp.get("x");
+        System.out.println("x: " + x);
+    }
+  }
+}
