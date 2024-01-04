c36ef5f3075630ad11a4749d05bcc9b97d8547ee
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-04 09:44:20 -0300

    feat: create scriptArrayArg

---
+++ src/main/java/br/uefs/larsid/iot/soft/scriptArrayArg.py
@@ -0,0 +1,24 @@
+import argparse
+import ast
+
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser(description='Exemplo de criação de array NumPy a partir de uma lista de valores.')
+
+    # Adicione argumentos aqui
+    parser.add_argument('valores', type=str, help='Valores para criar o array NumPy como uma lista')
+
+    args = parser.parse_args()
+
+    # Converte a string para uma lista utilizando ast.literal_eval
+    valores_lista = ast.literal_eval(args.valores)
+
+    # Cria um array NumPy a partir dos valores fornecidos
+    numpy_array = np.array(valores_lista)
+
+    print('Array NumPy criado:')
+    print(numpy_array)
+
+if __name__ == "__main__":
+    main()
