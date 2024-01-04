3cb7e5a3d5afa8599cbd3c2eafcd29aba7c295db
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-04 09:26:42 -0300

    feat: create python scripts

---
+++ src/main/java/br/uefs/larsid/iot/soft/script.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print("Hello World")
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+result = np.matmul(a, b)
+
+print(result)
---
+++ src/main/java/br/uefs/larsid/iot/soft/scriptParams.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+def main():
+    parser = argparse.ArgumentParser(description='Exemplo de passagem de valores para um script Python.')
+    
+    # Adicione argumentos aqui
+    parser.add_argument('valor1', type=int, help='Primeiro valor')
+    parser.add_argument('valor2', type=int, help='Segundo valor')
+
+    args = parser.parse_args()
+
+    # Acesse os valores passados como argumentos
+    resultado = args.valor1 + args.valor2
+    print(f'O resultado da soma é: {resultado}')
+
+if __name__ == "__main__":
+    main()
