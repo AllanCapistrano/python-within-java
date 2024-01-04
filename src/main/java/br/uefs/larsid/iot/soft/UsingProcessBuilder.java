a12f5b76fcca51656f3103040adebe1df564d27b
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-04 10:19:38 -0300

    chore: renaming variables

--- src/main/java/br/uefs/larsid/iot/soft/UsingProcessBuilder.java
+++ src/main/java/br/uefs/larsid/iot/soft/UsingProcessBuilder.java
@@ -8,40 +8,30 @@ import java.io.InputStreamReader;
 public class UsingProcessBuilder {
 
   public static void main(String[] args) {
-    // String fileContent = PythonHandler.lerArquivo(
-    //   "src/main/java/br/uefs/larsid/iot/soft/script.py"
-    // );
-    // System.out.println(fileContent);
-
     ProcessBuilder processBuilder = new ProcessBuilder("python", "src/main/java/br/uefs/larsid/iot/soft/script.py");
     processBuilder.redirectErrorStream(true);
 
     try {
       Process process = processBuilder.start();
-      System.out.println(lerInputStream(process.getInputStream()));
+      System.out.println(readInputStream(process.getInputStream()));
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    // List<String> results = readProcessOutput(process.getInputStream());
   }
 
-  public static String lerInputStream(InputStream inputStream) {
-        StringBuilder conteudo = new StringBuilder();
+  public static String readInputStream(InputStream inputStream) {
+        StringBuilder content = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String linha;
+            String line;
 
-            // Use o BufferedReader para ler o conteúdo do InputStream linha por linha
-            while ((linha = reader.readLine()) != null) {
-                conteudo.append(linha).append("\n");
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
             }
         } catch (IOException e) {
-            // Trate a exceção se houver um erro de leitura
             e.printStackTrace();
         }
 
-        // Retorne a String contendo o conteúdo lido do InputStream
-        return conteudo.toString();
+        return content.toString();
     }
 }
